431c024b90919fb9c868614473befdb7ae44355f
Ananya Yaduvanshi <133091353+Ananya-pro@users.noreply.github.com>
2023-11-15 19:00:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
- 
+  
 function highlight() {
     //Write your code here
   var boldWords = document.querySelectorAll('strong')
   boldWords.forEach(function(word) {
     word.style.color = 'green';
-   });
+  });
 }
 function return_normal() {
    var boldWords = document.querySelectorAll('strong')
   boldWords.forEach(function(word) {
     word.style.color = 'black';
   });
-}
 }
